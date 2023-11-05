bea8379c9f5e87979c49bb3c8177abe2b5978108
Rajarshi Dutta <gogolmf74@gmail.com>
2023-11-05 19:58:13 +0530

    userRoutes and User conflicts resolved

--- models/User.js
+++ models/User.js
@@ -14,38 +14,10 @@ const userSchema = new mongoose.Schema({
         type :String,
         required : true
     },
-<<<<<<< HEAD
     rating: {
         type: Number,
         required: true
     },
-=======
-    // problems : [
-    //     {
-    //         problemId : {
-    //             type : mongoose.Schema.Types.ObjectId,
-    //             ref : Problem,
-    //             required : true
-    //         },
-    //         status : {
-    //             type : String,
-    //             default : "Unsolved"
-    //         },
-    //         language: {
-    //             type : String,
-    //             default : 'c++'
-    //         },
-    //         code : {
-    //             type : String,
-    //             required : true
-    //         }
-    //     }
-    // ],
-    // rating: {
-    //     type: Number,
-    //     required: true
-    // },
->>>>>>> abffd2db0b859f3e0d1418bd1f766fe8e3933c9e
     solvedProblems: {
         hard: {
             type: Number,
@@ -59,19 +31,6 @@ const userSchema = new mongoose.Schema({
             type: Number,
             default : 0
         },
-<<<<<<< HEAD
-        problems: [{
-            id: {
-                type: mongoose.Schema.Types.ObjectId
-            },
-            title: {
-                type: String
-            },
-            difficulty: {
-                type: String
-            }
-        }]
-=======
         problems: [
         {
            problemId : {
@@ -94,9 +53,7 @@ const userSchema = new mongoose.Schema({
              }
            }]
         }
-    ]
->>>>>>> abffd2db0b859f3e0d1418bd1f766fe8e3933c9e
-    },
+    ]},
     streak: {
         type: Number,
         default : 0
@@ -104,13 +61,6 @@ const userSchema = new mongoose.Schema({
     last_potd : {
         type : Date
     }
-<<<<<<< HEAD
-=======
-
-
-    
-
->>>>>>> abffd2db0b859f3e0d1418bd1f766fe8e3933c9e
 })
 
 module.exports = mongoose.model ('User',userSchema)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,7 @@ const userController =require ( '../controllers/userController')
 
 router.route('/')
     .post(userController.createNewUser)
-<<<<<<< HEAD
-
-=======
     .patch(userController.editProfile)
->>>>>>> abffd2db0b859f3e0d1418bd1f766fe8e3933c9e
 router.route('/login')
     .post(userController.getUser)
     .get(userController.autoLogin)
